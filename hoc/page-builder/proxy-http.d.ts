529f403e8544fc212c79f7d42a9aa9e1c8fe82d5
xiang ni <xiang_x_ni@manulife.com>
2023-05-31 13:42:22 +0800

    update

--- hoc/page-builder/proxy-http.d.ts
+++ hoc/page-builder/proxy-http.d.ts
@@ -8,11 +8,11 @@ export declare class ProxyHttp {
     private page;
     constructor(http: HttpClient, jc: JsonConfigService, page: any);
     private proxyLoading;
-    get(req: RequestInfo | string, params?: RequestInit): Observable<any>;
+    get(req: RequestInfo | string, params?: RequestInit): Observable<unknown>;
     getText(req: RequestInfo | string, params?: RequestInit): Observable<string>;
     getJsonConfig(url: string): Observable<object>;
-    post(req: RequestInfo | string, params?: RequestInit): Observable<string>;
-    put(req: RequestInfo | string, params?: RequestInit): Observable<string>;
-    delete(req: RequestInfo | string, params?: RequestInit): Observable<string>;
+    post(req: RequestInfo | string, params?: RequestInit): Observable<unknown>;
+    put(req: RequestInfo | string, params?: RequestInit): Observable<unknown>;
+    delete(req: RequestInfo | string, params?: RequestInit): Observable<unknown>;
     request(method: string, req: RequestInfo | string, params?: RequestInit): Observable<Response>;
 }
