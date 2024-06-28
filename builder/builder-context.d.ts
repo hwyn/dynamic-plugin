ecaefd635ddb6638201b0bee8dac62a0ed14e877
xiang ni <xiang_x_ni@manulife.com>
2024-06-28 14:17:50 +0800

    update

--- builder/builder-context.d.ts
+++ builder/builder-context.d.ts
@@ -1,4 +1,4 @@
-import { BaseConvert, BasicExtension, BuilderContext } from '@dynamic/builder';
+import { BuilderContext } from '@dynamic/builder';
 import { InjectorToken, Type } from '@fm/di';
 export declare const VALIDATOR: InjectorToken;
 export declare const FORWARD_MICRO: InjectorToken;
@@ -9,10 +9,10 @@ export declare const builderPackage: (baseName: string, parent?: BuilderContext)
     forwardUiElement: (name: string, Element: any) => any;
     forwardHocComponent: (token: InjectorToken, hot: any) => any;
     UIElement: (name: string) => <T>(Type: Type<T>) => any;
-    ControlIntercept: <T_1 = any>(this: unknown, ...args: any[]) => (cls: Type<T_1>) => any;
-    Convert: <T_2 extends Type<BaseConvert>>(this: unknown, ...args: any[]) => (cls: Type<BaseConvert>) => any;
-    Action: (this: unknown, ...args: any[]) => (cls: Type<unknown>) => any;
-    Validator: <T_3 = any>(this: unknown, ...args: any[]) => (cls: Type<T_3>) => any;
-    Extension: <T_4 extends Type<BasicExtension>>(this: unknown, ...args: any[]) => (cls: Type<BasicExtension>) => any;
+    ControlIntercept: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction;
+    Convert: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction;
+    Action: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction;
+    Validator: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction;
+    Extension: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction;
 };
-export declare const builderContext: BuilderContext, forwardUiElement: (name: string, Element: any) => any, forwardHocComponent: (token: InjectorToken, hot: any) => any, UIElement: (name: string) => <T>(Type: Type<T>) => any, Validator: <T = any>(this: unknown, ...args: any[]) => (cls: Type<T>) => any, ControlIntercept: <T = any>(this: unknown, ...args: any[]) => (cls: Type<T>) => any, Convert: <T extends Type<BaseConvert>>(this: unknown, ...args: any[]) => (cls: Type<BaseConvert>) => any, Action: (this: unknown, ...args: any[]) => (cls: Type<unknown>) => any, Extension: <T extends Type<BasicExtension>>(this: unknown, ...args: any[]) => (cls: Type<BasicExtension>) => any;
+export declare const builderContext: BuilderContext, forwardUiElement: (name: string, Element: any) => any, forwardHocComponent: (token: InjectorToken, hot: any) => any, UIElement: (name: string) => <T>(Type: Type<T>) => any, Validator: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction, ControlIntercept: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction, Convert: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction, Action: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction, Extension: (...args: any[]) => <TFunction extends (new (...args: any[]) => any) & Type<any>>(target: TFunction) => TFunction;
