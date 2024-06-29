08f10d49e0608487fb6e0b46774e5708f795972a
xiang ni <xiang_x_ni@manulife.com>
2024-06-29 09:46:45 +0800

    update

--- builder/builder-context.d.ts
+++ builder/builder-context.d.ts
@@ -9,10 +9,10 @@ export declare const builderPackage: (baseName: string, parent?: BuilderContext)
     forwardUiElement: (name: string, Element: any) => any;
     forwardHocComponent: (token: InjectorToken, hot: any) => any;
     UIElement: (name: string) => <T>(Type: Type<T>) => any;
-    ControlIntercept: () => import("../../di/decorators").ClassDecorator;
-    Convert: () => import("../../di/decorators").ClassDecorator;
-    Action: (name: string, options?: any) => import("../../di/decorators").ClassDecorator;
-    Validator: (name: string) => import("../../di/decorators").ClassDecorator;
-    Extension: () => import("../../di/decorators").ClassDecorator;
+    ControlIntercept: () => ClassDecorator;
+    Convert: () => ClassDecorator;
+    Action: (name: string, options?: any) => ClassDecorator;
+    Validator: (name: string) => ClassDecorator;
+    Extension: () => ClassDecorator;
 };
-export declare const builderContext: BuilderContext, forwardUiElement: (name: string, Element: any) => any, forwardHocComponent: (token: InjectorToken, hot: any) => any, UIElement: (name: string) => <T>(Type: Type<T>) => any, Validator: (name: string) => import("../../di/decorators").ClassDecorator, ControlIntercept: () => import("../../di/decorators").ClassDecorator, Convert: () => import("../../di/decorators").ClassDecorator, Action: (name: string, options?: any) => import("../../di/decorators").ClassDecorator, Extension: () => import("../../di/decorators").ClassDecorator;
+export declare const builderContext: BuilderContext, forwardUiElement: (name: string, Element: any) => any, forwardHocComponent: (token: InjectorToken, hot: any) => any, UIElement: (name: string) => <T>(Type: Type<T>) => any, Validator: (name: string) => ClassDecorator, ControlIntercept: () => ClassDecorator, Convert: () => ClassDecorator, Action: (name: string, options?: any) => ClassDecorator, Extension: () => ClassDecorator;
